485ddb4a01f91669e38cc734d60e9e745afa7108
whguddnr9 <78585609+whguddnr9@users.noreply.github.com>
2024-07-02 22:08:48 +0900

    feat: api틀 작성

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, jsonify, request
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 from datetime import datetime, timedelta
+from pymongo import MongoClient
+
+#pymongo 변수 선언
+client = MongoClient('localhost',27017)
+db = client.jungle
+
 app = Flask(__name__)
 
 admin_id = "hjo"
@@ -34,10 +40,10 @@ def protected():
 #가입창 클릭시 가입화면전환
 @app.route("/register", methods=["POST"])
 def register():
-    return render_template('register.html')
+    return render_template('signup.html')
 
 #가입화면에서 가입버튼 클릭 시 폼 전달
-@app.route('/api/register', methods=['POST'])
+@app.route('/api/signup', methods=['POST'])
 def api_register():
     # 전달받은 request form 파싱
     reg_id     = request.form['id'] 
@@ -47,17 +53,51 @@ def api_register():
    #비밀번호 암호화 코드 필요
       
    #디비연동
-
+   #아이디와 패스워드 이름을 가입자 테이블에 추가
     return jsonify({'result':'success'})
 
+#질문 폼 api
 @app.route("/question",methods=["POST"])
 def question():
     #입력받은 폼 파싱
-    s_id    = request.form['s_id']
+    u_id    = request.form['s_id']
     msg     = request.form['msg']
     # 디비 연동
-
+    # 수신자와 질문내용 그리고 처리되지 않았다는 의미의 플래그를 질문 테이블에 저장
     return jsonify({"result":"success"})
+
+#받은 메시지 출력 api
+@app.route("/receive", methods=["GET"])
+def recevedMsg():
+    u_id = request.args.get("u_id")
+
+   #디비연동코드
+   #질문테이블에서 본인 아이디의 받은 메시지를 select
+    return jsonify({'result':'success','data':data})
+
+#받은 메시지 관리자 api
+@app.route("/adminMsg", methods=["POST"])
+def adminMsg() :
+   #디비연동코드
+   #질문테이블에서 모든 메시지를 select
+    return jsonify({'result':'success','data':data})
+ 
+#메시지 읽음처리 api
+@app.route('/receive/msgRead', methods=["POST"])
+def msgRead():
+    
+    return jsonify({'result':'success'})
+
+#메시지 승인 거절 api
+@app.route('/adminMsg/check', methods=['POST'])
+def msgCheck():
+    m_id    = request.form['m_id']
+    status  = request.form['status']
+
+    #디비 연동 코드
+    #디비에 해당 메시지 상태 업데이트
+    
+    return jsonify({'status':'success'})
 
 
 
