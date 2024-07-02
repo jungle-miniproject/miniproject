a2610d5888c84cd6c22397bfba2ebce88f8e1417
whguddnr9 <78585609+whguddnr9@users.noreply.github.com>
2024-07-02 20:17:10 +0900

    feat: 회원가입,질문 api 작성(db코드 제외)

--- app.py
+++ app.py
@@ -13,10 +13,9 @@ jwt = JWTManager(app)
 # create_access_token() function is used to actually generate the JWT.
 @app.route("/loginJWT", methods=["POST"])
 def login():
-    input_data=request.form
-    username = input_data['id']
-    password = input_data['pw']
-    if username != admin_id or password != admin_pw:
+    username = request.form['id'] # request.form에서 아이디와 패스워드 파싱
+    password = request.form['pwd']
+    if username != admin_id or password != admin_pw: # 만약 아이디와 비밀번호가 다를 시 에러 처리
         return jsonify({"msg": "Bad username or password"}), 401
 
     access_token = create_access_token(identity=username)
@@ -31,6 +30,36 @@ def protected():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
+
+#가입창 클릭시 가입화면전환
+@app.route("/register", methods=["POST"])
+def register():
+    return render_template('register.html')
+
+#가입화면에서 가입버튼 클릭 시 폼 전달
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    # 전달받은 request form 파싱
+    reg_id     = request.form['id'] 
+    reg_pwd    = request.form['pwd']
+    reg_email  = request.form['email']
+
+   #비밀번호 암호화 코드 필요
+      
+   #디비연동
+
+    return jsonify({'result':'success'})
+
+@app.route("/question",methods=["POST"])
+def question():
+    #입력받은 폼 파싱
+    s_id    = request.form['s_id']
+    msg     = request.form['msg']
+    # 디비 연동
+
+    return jsonify({"result":"success"})
+
+
 
 @app.route('/')
 def home():
