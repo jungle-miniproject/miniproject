16dffd99d6f6d8645900b109b2793547dba1b527
whguddnr9 <78585609+whguddnr9@users.noreply.github.com>
2024-07-03 20:06:27 +0900

    로그인 코드 수정

--- static/js/login/index.js
+++ static/js/login/index.js
@@ -1,36 +1,36 @@
-const idInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
-const loginButton = document.getElementById('loginBtn');
-const signupBtn = document.getElementById('signup');
+const idInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const loginButton = document.getElementById("loginBtn");
+const signupBtn = document.getElementById("signup");
 
-loginButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    postLogin();
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  postLogin();
 });
 
-signupBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.location.href = '/signup';
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = "/signup";
 });
 
-const postLogin = async () => {
-    const userId = idInput.value;
-    const userPwd = passwordInput.value;
-    const response = await fetch('/loginJWT', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id: userId, pwd: userPwd }),
-    });
-    console.log(response);
-    if (response.ok) {
-        let token = response['token'];
-        console.log(token);
-        localStorage.setItem('token', token);
-        console.log('response', response);
-        window.location.href = '/homepage';
-    } else {
-        alert('아이디 또는 비밀번호가 틀렸습니다.');
-    }
+const postLogin = () => {
+  const userId = idInput.value;
+  const userPwd = passwordInput.value;
+  $.ajax({
+    type: "POST",
+    url: "/loginJWT",
+    contentType: "application/json",
+    data: JSON.stringify({ id: userId, pwd: userPwd }),
+    success: function (response) {
+      console.log(response);
+      let access_token = response["token"];
+      console.log(access_token);
+      if (response["result"] == "success") {
+        localStorage.setItem("token", access_token);
+        window.location.href = "/homepage";
+      } else {
+        alert(response["msg"]);
+      }
+    },
+  });
 };
